6d78eb3dbcfbac415e38012b38707567689ca0fb
noracami <kerke.2011@gmail.com>
2024-01-03 01:47:17 +0800

    📝 Sec. 4 Primitives

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello Rust!");
+    let cats: u8 = 100;
+
+    if cats > 1 {
+        println!("好多貓");
+    } else {
+        println!("一隻貓");
+    }
+}
+
+fn variable_declare_convention() {
+    let hasCat = true;
+    const age: u8 = 20;
 }
